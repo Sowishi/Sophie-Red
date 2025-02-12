e8e79e96904c3138a72b2f399f8e8abe09fa0a2e
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2025-02-12 11:47:30 +0800

    I will do the check in part

--- src/components/bookingCard.jsx
+++ src/components/bookingCard.jsx
@@ -28,9 +28,10 @@ const BookingCard = ({ booking, onViewDetails }) => {
       )}
       <Card
         style={{
-          backgroundColor: "rgb(230,53,53)",
           backgroundImage:
-            "linear-gradient(360deg, rgba(230,53,53,1) 0%, rgba(254,254,254,1) 32%)",
+            booking.bookType == "event"
+              ? "linear-gradient(360deg, rgba(207,250,254,),rgba(0,0,230,1) 0%, rgba(254,254,254,1) 32%)"
+              : "linear-gradient(360deg, rgba(230,53,53,1) 0%, rgba(254,254,254,1) 32%)",
         }}
         className="max-w-sm my-3 h-[360px] mx-3"
       >
