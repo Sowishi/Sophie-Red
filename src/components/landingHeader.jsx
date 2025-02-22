a8112f936fec869c2cc994a64e0e309fddbf6d22
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2025-02-23 02:24:44 +0800

    Make the alignment of the landing header

--- src/components/landingHeader.jsx
+++ src/components/landingHeader.jsx
@@ -18,139 +18,143 @@ const LandingHeader = ({
         </h1>
       </Navbar.Brand>
       <Navbar.Toggle />
-      <Navbar.Collapse>
-        <Navbar.Link
-          className="text-black lg:text-white text-lg opacity-80 hover:bg-red-500"
-          href="/"
-        >
-          <span className="hover:text-red-500">Home</span>
-        </Navbar.Link>
-        <Navbar.Link
-          className="text-black lg:text-white text-lg opacity-80 hover:bg-red-500"
-          href="#testimonials"
-        >
-          Testimonials
-        </Navbar.Link>
-
-        <Navbar.Link
-          className="text-black lg:text-white text-lg opacity-80 hover:bg-red-500"
-          href="#rooms"
-        >
-          Rooms
-        </Navbar.Link>
-        <Navbar.Link
-          className="text-black lg:text-white text-lg opacity-80 hover:bg-red-500"
-          href="#Amenities"
-        >
-          Amenities
-        </Navbar.Link>
-        {currentUser && (
-          <div className="relative  flex items-center justify-center">
-            <p className="font-medium text-black lg:text-white mr-5 text-lg">
-              {currentUser.name}
-            </p>
-            <Dropdown
-              inline
-              label={
-                <img
-                  className="w-[50px] h-[50px] rounded-full cursor-pointer"
-                  src={currentUser.photoURL}
-                  alt="User Avatar"
-                />
-              }
-            >
-              <Dropdown.Item>
-                <p className="font-medium text-nowrap">
-                  {currentUser.name || currentUser.fullName}
-                </p>
-              </Dropdown.Item>
-              <Dropdown.Item>
-                <p className="text-gray-500">{currentUser.email}</p>
-              </Dropdown.Item>
-              <Dropdown.Item>
-                <Button
-                  color="info"
-                  className="w-full text-left"
-                  onClick={() => navigation("/client-dashboard")}
-                >
-                  Dashboard
-                </Button>
-              </Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item>
-                <Button
-                  color="failure"
-                  className="w-full text-left"
-                  onClick={() => {
-                    logout();
-                    localStorage.removeItem("user");
-                  }}
-                >
-                  Logout
-                </Button>
-              </Dropdown.Item>
-            </Dropdown>
-          </div>
-        )}
-
-        {currentAdmin && (
-          <div className="relative  flex items-center justify-center">
-            <p className="font-medium text-black lg:text-white mr-5 text-lg">
-              {currentAdmin.fullName}
-            </p>
-
-            <Dropdown
-              inline
-              label={
-                <img
-                  className="w-[50px] h-[50px] rounded-full cursor-pointer"
-                  src={currentAdmin.photoURL}
-                  alt="User Avatar"
-                />
-              }
-            >
-              <Dropdown.Item>
-                <p className="font-medium text-nowrap">
-                  {currentAdmin.name || currentAdmin.fullName}
-                </p>
-              </Dropdown.Item>
-              <Dropdown.Item>
-                <p className="text-gray-500">{currentAdmin.email}</p>
-              </Dropdown.Item>
-              <Dropdown.Item>
-                <Button
-                  color="info"
-                  className="w-full text-left"
-                  onClick={() => navigation("/dashboard")}
-                >
-                  Dashboard
-                </Button>
-              </Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item>
-                <Button
-                  color="failure"
-                  className="w-full text-left"
-                  onClick={() => {
-                    setCurrentAdmin(null);
-                    localStorage.removeItem("user");
-                  }}
-                >
-                  Logout
-                </Button>
-              </Dropdown.Item>
-            </Dropdown>
-          </div>
-        )}
-        {!currentUser && !currentAdmin && (
-          <Button
-            onClick={() => navigation("/login")}
-            className="px-5"
-            gradientMonochrome="info"
+      <Navbar.Collapse className="flex items-center w-full">
+        <div className="flex items-center gap-x-5">
+          <Navbar.Link
+            className="text-black lg:text-white text-lg opacity-80 hover:text-red-500"
+            href="/"
           >
-            Login
-          </Button>
-        )}
+            Home
+          </Navbar.Link>
+          <Navbar.Link
+            className="text-black lg:text-white text-lg opacity-80 hover:text-red-500"
+            href="#testimonials"
+          >
+            Testimonials
+          </Navbar.Link>
+          <Navbar.Link
+            className="text-black lg:text-white text-lg opacity-80 hover:text-red-500"
+            href="#rooms"
+          >
+            Rooms
+          </Navbar.Link>
+          <Navbar.Link
+            className="text-black lg:text-white text-lg opacity-80 hover:text-red-500"
+            href="#Amenities"
+          >
+            Amenities
+          </Navbar.Link>
+        </div>
+
+        <div className="ml-auto flex items-center gap-x-5">
+          {currentUser && (
+            <div className="flex items-center gap-x-3">
+              <p className="font-medium text-black lg:text-white text-lg">
+                {currentUser.name}
+              </p>
+              <Dropdown
+                inline
+                label={
+                  <img
+                    className="w-[50px] h-[50px] rounded-full cursor-pointer"
+                    src={currentUser.photoURL}
+                    alt="User Avatar"
+                  />
+                }
+              >
+                <Dropdown.Item>
+                  <p className="font-medium text-nowrap">
+                    {currentUser.name || currentUser.fullName}
+                  </p>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <p className="text-gray-500">{currentUser.email}</p>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <Button
+                    color="info"
+                    className="w-full text-left"
+                    onClick={() => navigation("/client-dashboard")}
+                  >
+                    Dashboard
+                  </Button>
+                </Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item>
+                  <Button
+                    color="failure"
+                    className="w-full text-left"
+                    onClick={() => {
+                      logout();
+                      localStorage.removeItem("user");
+                    }}
+                  >
+                    Logout
+                  </Button>
+                </Dropdown.Item>
+              </Dropdown>
+            </div>
+          )}
+
+          {currentAdmin && (
+            <div className="flex items-center gap-x-3">
+              <p className="font-medium text-black lg:text-white text-lg">
+                {currentAdmin.fullName}
+              </p>
+              <Dropdown
+                inline
+                label={
+                  <img
+                    className="w-[50px] h-[50px] rounded-full cursor-pointer"
+                    src={currentAdmin.photoURL}
+                    alt="User Avatar"
+                  />
+                }
+              >
+                <Dropdown.Item>
+                  <p className="font-medium text-nowrap">
+                    {currentAdmin.name || currentAdmin.fullName}
+                  </p>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <p className="text-gray-500">{currentAdmin.email}</p>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <Button
+                    color="info"
+                    className="w-full text-left"
+                    onClick={() => navigation("/dashboard")}
+                  >
+                    Dashboard
+                  </Button>
+                </Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item>
+                  <Button
+                    color="failure"
+                    className="w-full text-left"
+                    onClick={() => {
+                      setCurrentAdmin(null);
+                      localStorage.removeItem("user");
+                    }}
+                  >
+                    Logout
+                  </Button>
+                </Dropdown.Item>
+              </Dropdown>
+            </div>
+          )}
+
+          {!currentUser && !currentAdmin && (
+            <Button
+              onClick={() => navigation("/login")}
+              className="px-5"
+              gradientMonochrome="info"
+            >
+              Login
+            </Button>
+          )}
+        </div>
       </Navbar.Collapse>
     </Navbar>
   );
